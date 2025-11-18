9647bfb9ce26ad65387be30680b0255f0e8a1695
Jerico Christianto <jericochristianto@gmail.com>
2025-11-18 21:12:20 +0700

    Refactor Car and ElectricCar classes with properties

--- src/main/kotlin/com/dicoding/exam/exam4/Exam4.kt
+++ src/main/kotlin/com/dicoding/exam/exam4/Exam4.kt
@@ -1,11 +1,15 @@
 package com.dicoding.exam.exam4
 
 // TODO
-open class Car() {
-    open fun getCarInfo(): String
+open class Car(val brand: String, val year: Int) {
+    open fun getCarInfo(): String {
+        return "$brand keluaran tahun $year"
+    }
 }
 
 // TODO
-class ElectricCar() : Car() {
-    override fun getCarInfo(): String
+class ElectricCar(brand: String, year: Int, val batteryCapacity: Int) : Car(brand, year) {
+    override fun getCarInfo(): String {
+        return "$brand keluaran tahun $year dengan kapasitas baterai $batteryCapacity kWh"
+    }
 }
