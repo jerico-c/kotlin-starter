bc3e1d69f6e07e6b23b030970e2957488dc9fc37
Jerico Christianto <jericochristianto@gmail.com>
2025-11-19 08:49:01 +0700

    Implement string manipulation with number extraction

--- src/main/kotlin/com/dicoding/exam/optionalexam3/OptionalExam3.kt
+++ src/main/kotlin/com/dicoding/exam/optionalexam3/OptionalExam3.kt
@@ -2,5 +2,14 @@ package com.dicoding.exam.optionalexam3
 
 // TODO
 fun manipulateString(str: String, int: Int): String {
-    return ""
+    val numberRegex = Regex("[0-9]+")
+    val match = numberRegex.find(str)
+
+    return if (match != null) {
+        val number = match.value.toInt()
+        val newNumber = number * int
+        str.replace(match.value, newNumber.toString())
+    } else {
+        str + int
+    }
 }
