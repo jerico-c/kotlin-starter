409acb0ffb52b1d38b6993eec70bbd7e07141a3b
Jerico Christianto <jericochristianto@gmail.com>
2025-11-19 08:49:35 +0700

    Add getMiddleCharacters function
    Implement function to get middle characters of a string.

--- src/main/kotlin/com/dicoding/exam/optionalexam4/OptionalExam4.kt
+++ src/main/kotlin/com/dicoding/exam/optionalexam4/OptionalExam4.kt
@@ -2,5 +2,14 @@ package com.dicoding.exam.optionalexam4
 
 // TODO
 fun getMiddleCharacters(string: String): String {
-    return ""
+    val length = string.length
+    val mid = length / 2
+
+    return if (length % 2 == 0) {
+        // Even: return character at mid-1 and mid
+        string.substring(mid - 1, mid + 1)
+    } else {
+        // Odd: return character at mid
+        string[mid].toString()
+    }
 }
